ce0917e28ea7a66c4620f55d0118306202493b4d
Madhavan Neelakandan <Madhavan_N@ad.infosys.com>
2017-05-16 10:59:03 +0530

    added jms producer

--- todos/todoViewService/src/main/java/org/simplity/examples/JerseyEntryPoint.java
+++ todos/todoViewService/src/main/java/org/simplity/examples/JerseyEntryPoint.java
@@ -10,6 +10,7 @@ import javax.ws.rs.core.Response;
 
 import org.simplity.json.JSONObject;
 import org.simplity.service.JavaAgent;
+import org.simplity.service.PayloadType;
 import org.simplity.service.ServiceData;
 
 @Path("/entry-point")
@@ -33,5 +34,25 @@ public class JerseyEntryPoint {
 		//ServiceData outData = JavaAgent.getAgent("100", null).serve("helloworld", null);
 		
 		 return Response.ok(obj).build();
+    }
+	
+	@GET
+    @Path("/getJmsQueue")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getJmsQueue() {
+		System.out.println("JerseyEntryPoint");
+		ServiceData outData = JavaAgent.getAgent("100", null).serve("jmsConsumer", null);		
+		 return Response.ok(outData.getPayLoad()).build();
+    }
+	
+	@GET
+    @Path("/putJmsQueue")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.TEXT_PLAIN})
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.TEXT_PLAIN})
+    public Response putJmsQueue() {
+		System.out.println("JerseyEntryPoint");
+		String data = "{'test':'Sample message'}";
+		ServiceData outData = JavaAgent.getAgent("100", null).serve("jmsProducer", data, PayloadType.JSON);
+		return Response.ok(outData.getPayLoad()).build();
     }
 }
