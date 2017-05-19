93d07727239dfe7b5463aefb7c6103505d04962c
Madhavan Neelakandan <Madhavan_N@ad.infosys.com>
2017-05-19 10:49:43 +0530

    todos - fixed issues

--- todos/todoViewService/src/main/java/org/simplity/examples/TodoViewService.java
+++ todos/todoViewService/src/main/java/org/simplity/examples/TodoViewService.java
@@ -1,16 +1,12 @@
 package org.simplity.examples;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.simplity.json.JSONObject;
 import org.simplity.service.JavaAgent;
-import org.simplity.service.PayloadType;
 import org.simplity.service.ServiceData;
 
 @Path("/todos")
