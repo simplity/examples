db3c8a164f4ac6b4bd9f301a504de4a4cdd46d0a
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-05-23 12:11:02 +0530

    TodoUI as jetty app

--- todos/todosUI/src/main/java/org/simplity/examples/TodosUIMain.java
+++ todos/todosUI/src/main/java/org/simplity/examples/TodosUIMain.java
@@ -1,22 +1,30 @@
 package org.simplity.examples;
 
+import java.net.URL;
+
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 
 public class TodosUIMain {
 
 	public static void main(String[] args) {
-		ContextHandlerCollection contexts = new ContextHandlerCollection();
 		
-		contexts.setHandlers(new Handler[] 
-			{ new AppContextBuilder().buildWebAppContext()});
+		String baseStr = "/webapp";
+		URL baseUrl = TodosUIMain.class.getResource(baseStr);
+		String basePath = baseUrl.toExternalForm();
+
+		ResourceHandler resourceHandler = new ResourceHandler();
+		resourceHandler.setResourceBase(basePath);
+		resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
 		
-		final JettyServer server = new JettyServer();
-		server.setHandler(contexts);
+		final Server server = new Server();
+		server.setHandler(resourceHandler);
 		try {
 			server.start();
+			server.join();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
