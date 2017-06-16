f107a1dde5e513b7fbe3a89abe2016ab1423529d
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-06-16 18:13:50 +0530

    load DB datafrom js controller

--- troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/app.js
+++ troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/app.js
@@ -9,7 +9,7 @@ angular.module('myApp.controllers', []);
 
 angular.module('myApp', ['myApp.filters',  'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'TroubleTicketCtrl'});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'TBD1Ctrl'});
     $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'TBD2Ctrl'});
     $routeProvider.otherwise({redirectTo: '/view1'});
--- troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
+++ troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
@@ -5,7 +5,7 @@
 angular.module('myApp.controllers')
 
   .controller('TroubleTicketCtrl', ['$scope','$http',function($scope, $http) {
-	  $scope.mydata = [];
+	  $scope.tickets = [];
 	  
 	  $scope.loadDB = function(){
 		var data =  [ {
@@ -20,10 +20,10 @@ angular.module('myApp.controllers')
 			  "description" : "videoQuailty",
 			  "severity" : "High",
 			  "type" : "DSPDailymotion",
-			  "creationDate" : "2013-10-20 18:44:27",
+			  "creationDate" : new Date("2013-10-20 18:44:27"),
 			  "status" : "InProgress_Pending",
 			  "statusChangeReason" : "Access Seeker action/information required",
-			  "statusChangeDate" : "2013-10-20 18:44:36",
+			  "statusChangeDate" : new Date("2013-10-20 18:44:36"),
 			  "notes" : [ ],
 			  "relatedpartyref" : [ {
 			    "role" : "user",
@@ -34,20 +34,20 @@ angular.module('myApp.controllers')
 				  "description" : "Some Description",
 				  "severity" : "Medium",
 				  "type" : "Bills, charges or payment",
-				  "creationDate" : "2013-09-16 16:39:28",
-				  "targetResolutionDate" : "2013-09-16 16:39:28",
+				  "creationDate" : new Date("2013-09-16 16:39:28"),
+				  "targetResolutionDate" : new Date("2013-09-16 16:39:28"),
 				  "status" : "Acknowledged",
-				  "resolutionDate" : "2013-09-16 16:39:28",
+				  "resolutionDate" : new Date("2013-09-16 16:39:28"),
 				  "relatedobject" : [ {
 				    "reference" : "referenceobject",
 				    "involvement" : "involvment"
 				  } ],
 				  "note" : [ {
-				    "date" : "2013-09-16 16:39:28",
+				    "date" : new Date("2013-09-16 16:39:28"),
 				    "author" : "author",
 				    "text" : "text"
 				  }, {
-				    "date" : "2013-09-16 16:39:28",
+				    "date" : new Date("2013-09-16 16:39:28"),
 				    "author" : "author",
 				    "text" : "text"
 				  } ],
@@ -63,7 +63,7 @@ angular.module('myApp.controllers')
 			for(var i=0;i<data.length;i++){
 				$http.post('http://localhost:8081/api/troubleTicket',JSON.stringify(data[i]))
 				.then(function (json) {
-					$scope.mydata = json;
+					console.log("Data loaded to DB");
 				}, function () {
 					alert("error");
 				});
@@ -73,22 +73,9 @@ angular.module('myApp.controllers')
 			
 	  $http.get('http://localhost:8081/api/troubleTicket')
 		.then(function (json) {
-			$scope.mydata = json;
+			$scope.tickets = json.tickets;
 		}, function () {
 			alert("error");
 		});
     
-	  
-	  /*var troubleTicket = {
-			  "description":"videoQuailty",
-			  "severity":"High",
-			  "type":"DSPDailymotion",
-	  };
-	  
-	  $http.post('http://localhost:8081/api/troubleTicket',JSON.stringify(troubleTicket))
-		.then(function (json) {
-			$scope.mydata = json;
-		}, function () {
-			alert("error");
-		});*/
-  }]);
+ }]);
