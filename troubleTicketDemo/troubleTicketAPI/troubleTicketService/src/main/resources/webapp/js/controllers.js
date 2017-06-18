f0caa4499d1ac2f7284fb01dd35c853e4c11fc4d
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-06-18 11:28:27 +0530

    fix for view all trouble tickets

--- troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
+++ troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
@@ -4,78 +4,21 @@
 
 angular.module('myApp.controllers')
 
-  .controller('TroubleTicketCtrl', ['$scope','$http',function($scope, $http) {
-	  $scope.tickets = [];
-	  
-	  $scope.loadDB = function(){
-		var data =  [ {
-			  "id" : "1",
-			  "severity" : "High",
-			  "description" : "Complaint",
-			  "type" : "Product",
-			  "status" : "Resolved",
-			  "note" : [ ]
-			}, {
-			  "id" : "1000",
-			  "description" : "videoQuailty",
-			  "severity" : "High",
-			  "type" : "DSPDailymotion",
-			  "creationDate" : new Date("2013-10-20 18:44:27"),
-			  "status" : "InProgress_Pending",
-			  "statusChangeReason" : "Access Seeker action/information required",
-			  "statusChangeDate" : new Date("2013-10-20 18:44:36"),
-			  "notes" : [ ],
-			  "relatedpartyref" : [ {
-			    "role" : "user",
-			    "href" : "vbo"
-			  }]
-			},{
-				  "id" : "1004",
-				  "description" : "Some Description",
-				  "severity" : "Medium",
-				  "type" : "Bills, charges or payment",
-				  "creationDate" : new Date("2013-09-16 16:39:28"),
-				  "targetResolutionDate" : new Date("2013-09-16 16:39:28"),
-				  "status" : "Acknowledged",
-				  "resolutionDate" : new Date("2013-09-16 16:39:28"),
-				  "relatedobject" : [ {
-				    "reference" : "referenceobject",
-				    "involvement" : "involvment"
-				  } ],
-				  "note" : [ {
-				    "date" : new Date("2013-09-16 16:39:28"),
-				    "author" : "author",
-				    "text" : "text"
-				  }, {
-				    "date" : new Date("2013-09-16 16:39:28"),
-				    "author" : "author",
-				    "text" : "text"
-				  } ],
-				  "relatedpartyref" : [ {
-				    "role" : "role",
-				    "href" : "reference party"
-				  }, {
-				    "role" : "role",
-				    "href" : "reference party"
-				  } ]
-				}
-			];
-			for(var i=0;i<data.length;i++){
-				$http.post('http://localhost:8081/api/troubleTicket',JSON.stringify(data[i]))
-				.then(function (json) {
-					console.log("Data loaded to DB");
-				}, function () {
-					alert("error");
-				});
-			}  
-	  }
-			
-			
-	  $http.get('http://localhost:8081/api/troubleTicket')
-		.then(function (json) {
-			$scope.tickets = json.tickets;
-		}, function () {
-			alert("error");
-		});
+  .controller('TroubleTicketCtrl', ['$scope', '$http', function($scope, $http) {
+
+    // Instantiate an object to store scope data
+    $scope.myData = {};
+
+    $http.get("api/troubleTicket")
+    .then(function(response) {
+        $scope.myData.ticketList = response.data.tickets;
+    });
     
- }]);
+    $scope.setCurrentTicket = function(id) {
+    	 $http.get("api/troubleTicket/"+id)
+    	    .then(function(response) {
+    	        $scope.myData.currentTicket = response.data.ticket[0];
+    	    });    	  
+      };
+    
+  }])
