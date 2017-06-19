d1e98b0a5f227cfe354989dc92e2b48dddf0c17b
Chetana Amancharla <Chetana_Shanbhag@ad.infosys.com>
2017-06-19 12:28:16 +0530

    angular crud actions

--- troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
+++ troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
@@ -8,17 +8,42 @@ angular.module('myApp.controllers')
 
     // Instantiate an object to store scope data
     $scope.myData = {};
-
+    $scope.myData.action = "list";
     $http.get("api/troubleTicket")
     .then(function(response) {
         $scope.myData.ticketList = response.data.tickets;
     });
     
-    $scope.setCurrentTicket = function(id) {
+    $scope.setCurrentTicketView = function(id) {
     	 $http.get("api/troubleTicket/"+id)
     	    .then(function(response) {
+    	    	$scope.myData.action = "view";
     	        $scope.myData.currentTicket = response.data.ticket[0];
     	    });    	  
       };
     
+      $scope.setCurrentTicketEdit = function(id) {
+     	 $http.get("api/troubleTicket/"+id)
+     	    .then(function(response) {
+     	    	$scope.myData.action = "edit";
+     	        $scope.myData.currentTicket = response.data.ticket[0];
+     	    });    	  
+       };      
+    
+       $scope.createTicket = function(){
+     	 $scope.myData.currentTicket = null;    	   
+    	 $scope.myData.action = "edit"; 
+       };
+       
+       $scope.updateTicket = function(ticket){     	   
+         	 $http.put("api/troubleTicket/"+ticket.id,ticket) 
+    	    .then(function(response) {
+    	    	console.log(response);
+    	    }); 
+         };       
+       
+      $scope.showAll = function(){
+    	  $scope.myData.currentTicket = null;
+    	  $scope.myData.action = "list";  
+      };
   }])
