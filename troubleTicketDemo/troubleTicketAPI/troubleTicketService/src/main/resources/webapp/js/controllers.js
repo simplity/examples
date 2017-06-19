e91e184bcc59d334b1884c1948f91b760088fb8f
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-06-19 17:06:25 +0530

    add Ticket issue fixed

--- troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
+++ troubleTicketDemo/troubleTicketAPI/troubleTicketService/src/main/resources/webapp/js/controllers.js
@@ -8,6 +8,7 @@ angular.module('myApp.controllers')
 
     // Instantiate an object to store scope data
     $scope.myData = {};
+    $scope.ticketAction = "get";
     $scope.myData.action = "list";
     $http.get("api/troubleTicket")
     .then(function(response) {
@@ -26,6 +27,7 @@ angular.module('myApp.controllers')
      	 $http.get("api/troubleTicket/"+id)
      	    .then(function(response) {
      	    	$scope.myData.action = "edit";
+     	    	$scope.ticketAction = "update";
      	        $scope.myData.currentTicket = response.data.ticket[0];
      	    });    	  
        };      
@@ -33,13 +35,22 @@ angular.module('myApp.controllers')
        $scope.createTicket = function(){
      	 $scope.myData.currentTicket = null;    	   
     	 $scope.myData.action = "edit"; 
+    	 $scope.ticketAction = "add";
        };
        
-       $scope.updateTicket = function(ticket){     	   
+       $scope.updateTicket = function(ticket){     
+    	   if($scope.ticketAction == "update"){
          	 $http.put("api/troubleTicket/"+ticket.id,ticket) 
     	    .then(function(response) {
     	    	console.log(response);
     	    }); 
+    	   }
+    	   else if($scope.ticketAction == "add"){
+    		$http.post("api/troubleTicket",ticket) 
+    	    .then(function(response) {
+    	    	console.log(response);
+    	    });   
+    	   }
          };       
        
       $scope.showAll = function(){
