1c61556497774f9ba563727090ad23fe89435f95
Shalini Reddy Bommana <Shalinireddy_B@ad.infosys.com>
2017-06-22 17:40:22 +0530

    i/p and o/p data for jms issue fixed

--- troubleTicketDemo/carManufacturerService/src/main/resources/webapp/js/controllers.js
+++ troubleTicketDemo/carManufacturerService/src/main/resources/webapp/js/controllers.js
@@ -23,7 +23,7 @@ angular.module('myApp.controllers')
     	    });    	  
       };
     
-      $scope.setCurrentTicketEdit = function(id) {
+      $scope.setCurrentTicketEdit = function(id) {    	  
      	 $http.get("api/troubleTicket/"+id)
      	    .then(function(response) {
      	    	$scope.myData.action = "edit";
@@ -40,12 +40,14 @@ angular.module('myApp.controllers')
        
        $scope.updateTicket = function(ticket){     
     	   if($scope.ticketAction == "update"){
+    		 ticket.source = "self";  
          	 $http.put("api/troubleTicket/"+ticket.id,ticket) 
     	    .then(function(response) {
     	    	console.log(response);
     	    }); 
     	   }
     	   else if($scope.ticketAction == "add"){
+    		ticket.source = "self";    		   
     		$http.post("api/troubleTicket",ticket) 
     	    .then(function(response) {
     	    	console.log(response);
