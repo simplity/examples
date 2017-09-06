9577a8acb3ef611232a564c8853ea621ac2dbd90
Madhavan Neelakandan <Madhavan_N@ad.infosys.com>
2017-09-06 18:59:57 +0530

    added testcase for contractGet and contractFind

---
+++ contractDB/webapp/src/test/java/org/simplity/examples/ScdbTest.java
@@ -0,0 +1,20 @@
+package org.simplity.examples;
+
+import org.simplity.utils.SimpliTestCase;
+
+public class ScdbTest extends SimpliTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		applicationRoot = Thread.currentThread().getContextClassLoader().getResource("comp").getPath();
+		testuser = "100";
+		testpwd = "pwd";
+		skipValidation=true;
+		super.setUp();
+	}
+
+	public void testHello() {
+		servicetest = "contractTestRun";
+	}
+
+}
